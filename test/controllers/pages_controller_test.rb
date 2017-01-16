f287e6522e9b658bdf5713a33f128ce2854074ce
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-16 11:25:35 -0500

    Add test for page's title & update code to pass the test.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @name = "Tologon Eshimkanov"
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select 'title', @name
   end
 
 end
