2c32bbf07891c770bc8d677129912294f58bcf67
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-20 17:54:31 -0500

    Route recaptcha in the back end & add proper messages if couldn't verify.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,6 @@ class PagesController < ApplicationController
     send_file Rails.root.join('app', 'assets', 'pdf', 'resume_tologon_eshimkanov.pdf'), type: 'application/pdf', disposition: 'inline'
   end
 
-  # POST /contact
   def contact
     @email = params[:user][:message]
     if verify_recaptcha
