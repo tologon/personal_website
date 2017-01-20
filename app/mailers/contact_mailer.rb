af39853c86e622ad71df142b11997d64ff91d33f
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-19 22:15:23 -0500

    Configure ContactMailer.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,9 @@
 class ContactMailer < ApplicationMailer
+  default from: 'notifications@eshimkanov.com'
+
+  def new_message(user)
+    @user = user
+    @url = 'http://www.eshimkanov.com/'
+    mail(to: @user.email, subject: 'Test from eshimkanov.com')
+  end
 end
