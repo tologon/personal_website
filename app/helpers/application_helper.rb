c078c25221a3062d85f568455ef456e51c2a1481
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-31 16:29:25 -0500

    Replace header values.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Tologon Eshimkanov"
+    base_title = Settings.name
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
-  end
-
-  def header_menus(option = '')
-    header_menus = {intro: 'Intro', projects: 'Projects', contact: 'Contact'}
-    header_menus[option]
   end
 end
