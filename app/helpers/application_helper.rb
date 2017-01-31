038fffce7f45c021f255e74f278feb090bb9a96a
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-31 15:55:16 -0500

    Refactor full_title method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,7 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = "Tologon Eshimkanov"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
 
   def header_menus(option = '')
