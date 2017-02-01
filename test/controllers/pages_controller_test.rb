c0d40a9e7a44ec2c460734d8354f817da92e0a87
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-01-31 19:54:54 -0500

    Fix tests to account for Config gem.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
+Settings.reload_from_files(
+  Rails.root.join('config', 'settings.yml').to_s
+)
+
 class PagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
@@ -9,7 +13,7 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
-    assert_select 'title', @name
+    assert_select 'title', "Home | #{@name}"
     assert_select 'a.page-scroll', 'Projects'
     assert_select 'a.page-scroll', 'Contact'
   end
