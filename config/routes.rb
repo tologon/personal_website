20005e9425964ed1cd729d7605edacb2339d535b
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-02-05 16:12:51 -0500

    Configure routes & devise and add views & controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+  end
   get   'pages/contact'
   get   'pages/resume_pdf'
   get   'pages/home'
