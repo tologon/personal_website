90e466eca42f2aac11c40e5619be3eb09e8f36aa
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-02-06 16:13:05 -0500

    Fix failing test (devise-specific).

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class AdminControllerTest < ActionDispatch::IntegrationTest
   test "should get main" do
     get admin_main_url
-    assert_response :success
+    assert_redirected_to controller: 'users/sessions', action: 'new'
   end
 
 end
