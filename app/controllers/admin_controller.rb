366ab22b1a6b36c95420b290378145dd7de9672d
Tologon Eshimkanov <teshimkanov@gmail.com>
2017-02-06 16:09:51 -0500

    Add authentication check for the admin side.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+
   def main
     settings_path = Rails.root.join('config', 'settings.yml').to_s
     @settings = Psych.load_file(settings_path)
